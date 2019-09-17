41762dc4704bb45c605e4a3670831d815cf336a9
kkiyama117 <k.kiyama117@gmail.com>
2019-09-17 15:35:01 +0900

    Add nodemon

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
-import { NextFunction, Response, Request, Router } from 'express';
+import { Response, Request, Router } from 'express';
+// import { NextFunction, Response, Request, Router } from 'express';
 
 const router = Router();
 
 /* GET home page. */
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
+router.get('/', (req: Request, res: Response) => {
+  // router.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('index');
 });
 
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,13 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
+// import { NextFunction, Request, Response } from 'express';
 
 import express from 'express';
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req: Request, res: Response, next: NextFunction) =>
+router.get('/', (req: Request, res: Response) =>
+  // router.get('/', (req: Request, res: Response, next: NextFunction) =>
   res.send('respond with a resource')
 );
 

35a0a941ff4557760df761b6e9d38e3983c3e826
kkiyama117 <k.kiyama117@gmail.com>
2019-10-01 17:25:24 +0900

    Keep

--- ormconfig.ts
+++ ormconfig.ts
@@ -13,5 +13,32 @@ const ORMConfig = {
   },
 };
 
+const ORMConfigProd = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'test',
+  password: 'test',
+  database: 'test',
+  synchronize: true,
+  logging: false,
+  entities: ['src/entity/**/*.ts'],
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/entity',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber',
+  },
+};
+
 // typeORM の雑魚設定に合わせる
-export = ORMConfig;
+const env = process.env.NODE_ENV;
+
+let conf;
+if (env == 'production') {
+  conf = ORMConfigProd;
+} else {
+  conf = ORMConfig;
+}
+export = conf;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -5,6 +5,26 @@ import { checkRole } from '../middlewares/checkRole';
 
 const router = Router();
 
+/**
+ * @swagger
+ * definition:
+ *   User:
+ *     type: object
+ *     description: ユーザー情報
+ *     required:
+ *       - firstName
+ *       - lastName
+ *       - password
+ *     properties:
+ *       name:
+ *         type: string
+ *         description: ユーザー名
+ *       password:
+ *         type: string
+ *         format: password
+ *         description: パスワード
+ */
+
 //Get all users
 router.get('/', [checkJwt, checkRole(['ADMIN'])], UserController.listAll);
 
